530b9b73cb0fedddcffc49849d6b5bdd975ed77a
M-Renerg <mikael.k.renberg@hotmail.com>
2025-12-04 14:57:37 +0100

    Connection to server
    wrote the code for the server connection. added config.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using MySql.Data.MySqlClient;
+using server;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+string db = "server=127.0.0.1;uid=holidaymaker;pwd=holidaymaker;database=holidaymaker";
+Config config = new(db);
+builder.Services.AddSingleton<Config>(config);
+
+var app = builder.Build();
 
 app.Run();
