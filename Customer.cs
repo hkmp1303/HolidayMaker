2b546de5cd8908389102c762a356505f263c36d8
M-Renerg <mikael.k.renberg@hotmail.com>
2025-12-05 10:07:20 +0100

    did basics for customer.cs

--- Customer.cs
+++ Customer.cs
@@ -3,5 +3,9 @@ namespace server;
 
 class Customer
 {
-    
+    //public record customer(int id, string email, string password, string firstname, string lastname, string phonenumber, string address)
+    //this is just for me so that I can keep track of that a full record of customer would hold 
+
+    //customer insert quary 
+    public record 
 }
