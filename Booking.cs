303ba5d5b10a6220a976daf61f16333be4d3ba4e
M-Renerg <mikael.k.renberg@hotmail.com>
2025-12-10 10:40:50 +0100

    working on the booking system
    had to go over to main to pull lastest to help test code

--- Booking.cs
+++ Booking.cs
@@ -14,33 +14,99 @@ static class BookingRequest
 
     public static async Task CreateBooking(Booking credentials, Config config, HttpContext ctx)
     {
-        if(ctx.Session.IsAvailable)
+        if (ctx.Session.IsAvailable)
         {
             if(ctx.Session.GetInt32("user_id") is int user_id)
             {
+                using var con = new MySqlConnection(config.ConnectionString);
+                
+                var mycmd = new MySqlCommand();
+
+                await con.OpenAsync();
+
+                using var transaction = con.BeginTransaction();
+
+                
                 string bookingquery = """
-                INSERT INTO booking(fk_user_id, fk_transportation_id, bookingdate)
-                VALUES(@userid, @transportationid, NOW());
-                SELECT LAST_INSERT_ID();
-                """;
+                     INSERT INTO booking(fk_user_id, fk_transportation_id, bookingdate)
+                     VALUES(@userid, @transportationid, NOW())
+                     SELECT LAST_INSERT_ID();
+                     """;
 
                 int booking_id;
 
-                var cmd = new MySqlCommand();
+                using (var cmd = new MySqlCommand(bookingquery, con, transaction))
+                {
+                    cmd.Parameters.AddWithValue("@userid", user_id);
+                    cmd.Parameters.AddWithValue("@transportationid", credentials.transportationid);
 
-                booking_id = Convert.ToInt32(cmd.LastInsertedId);
+                    booking_id = Convert.ToInt32(cmd.LastInsertedId);
+                }
 
-                // var parameters = new MySqlParameter[]
-                // {
-                //   new("@userid", user_id),
-                //   new("@transportationid", credentials.transportationid)
-                  
-                // };
-                // await MySqlHelper.ExecuteReaderAsync(config.ConnectionString, bookingquery, parameters);
+                foreach(var br in credentials.roomlist)
+                {
+                    
+                }
+                
+                foreach(var ba in credentials.activityList)
+                {
+                    
+                }
             }
         }
-        
     }
+
+
+//     public static async Task CreateBooking(Booking credentials, Config config, HttpContext ctx)
+//     {
+//         if(ctx.Session.IsAvailable)
+//         {
+//             if(ctx.Session.GetInt32("user_id") is int user_id)
+//             {
+
+//                 var cmd = new MySqlCommand();
+
+//                 string bookingquery = """
+//                 INSERT INTO booking(fk_user_id, fk_transportation_id, bookingdate)
+//                 VALUES(@userid, @transportationid, NOW())
+//                 SELECT LAST_INSERT_ID();
+//                 """;
+
+//                  var parametersbooking = new MySqlParameter[]
+//                  {
+//                    new("@userid", user_id),
+//                    new("@transportationid", credentials.transportationid)  
+//                  };
+
+
+//                 await MySqlHelper.ExecuteReaderAsync(config.ConnectionString, bookingquery, parametersbooking);
+
+//                 int booking_id;
+
+//                 booking_id = Convert.ToInt32(cmd.LastInsertedId);
+
+//                 string bookingroomquary = """
+//                 INSERT INTO room(fk_hotel_id, fk_price_id, roomtype, status)
+//                 VALUES(@fkhotelid, @fkpriceid, @roomtype, @status)
+//                 SELECT LAST_INSERT_ID();
+//                 """;
+
+                
+
+//                 int room_id;
+
+//                 room_id = Convert.ToInt32(cmd.LastInsertedId);
+
+//                 string bookinghotelquary = """
+//                 INSERT INTO bookinghotel(fk_booking_id, fk_room_id, date_start, date_end)
+//                 VALUES (@fkbookingid, @fkroomid, @date_start, @date_end);
+//                 """;
+
+
+//             }
+//         }
+        
+//     }
 }
 
 /*
