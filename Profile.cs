38bfc189b91284a79b0e4b6e1a5acdab921733af
hkmp1303 <hkmp1303@gmail.com>
2025-12-12 16:37:30 +0100

    Refactor patch_UserData to complete additional password check and update password feild, Add error messaging when returning false
    Allow user to update password when logged in, facilitate error
    maintenance and follow up

--- Profile.cs
+++ Profile.cs
@@ -6,9 +6,8 @@ namespace server;
 static class Profile
 {
     public record Get_Data(string Email, string Firstname, string Lastname, string Phonenumber, string Address);
-    public record Update_Data(string? Email, string? Firstname, string? Lastname, string? Phonenumber, string? Address);
-    public static async Task<Get_Data?>
-    Get_UserData(Config config, HttpContext ctx)
+    public record Update_Data(string? Email, string? Firstname, string? Lastname, string? Phonenumber, string? Address, string? CurrentPass, string? NewPass);
+    public static async Task<Get_Data?> Get_UserData(Config config, HttpContext ctx)
     {
         Get_Data? result = null;
         if (!ctx.Session.IsAvailable)
@@ -22,17 +21,26 @@ static class Profile
         {
             if (r.Read())
             {
-                result = new(r.GetString(0), r.GetString(1), r.GetString(2), r.IsDBNull(3)? "" : r.GetString(3), r.GetString(4));
+                result = new(r.GetString(0), r.GetString(1), r.GetString(2), r.IsDBNull(3) ? "" : r.GetString(3), r.GetString(4));
             }
         }
         return result;
     }
-    public static async Task<bool> Patch_UserData(Config config, HttpContext ctx, Update_Data data)
+    public record updateUserDataResult(string status, string[]? errors = null);
+    public static async Task<updateUserDataResult> Patch_UserData(Config config, HttpContext ctx, Update_Data data)
     {
+        string status = "success";
+        List<string> errors = new();
         if (!ctx.Session.IsAvailable)
-            return false;
+        {
+            ctx.Response.StatusCode = 401;
+            return new updateUserDataResult("error", new string[] { "Not logged in" });
+        }
         if (ctx.Session.GetInt32("user_id") is not int user_id)
-            return false;
+        {
+            ctx.Response.StatusCode = 401;
+            return new updateUserDataResult("error", new string[] { "Invalid user session" });
+        }
 
         var parameters = new List<MySqlParameter>
         {
@@ -70,10 +78,38 @@ static class Profile
             userValues.Add("address = @address");
             parameters.Add(new MySqlParameter("@address", data.Address));
         }
-        if (parameters.Count == 1)
-            return false;
-        query += string.Join(',', userValues) + " WHERE userid = @id";
-        int updatedRows = await MySqlHelper.ExecuteNonQueryAsync(config.ConnectionString, query, parameters.ToArray());
-        return updatedRows > 0;
+
+        if (data.NewPass != null && data.CurrentPass != null)
+        {
+            string newPass = data.NewPass.Trim(), currentPass = data.CurrentPass.Trim();
+            if (newPass == "" || currentPass == "") {
+                errors.Add("Current or new password user input error");
+            }
+            if (newPass == currentPass)
+                errors.Add("New and current password are the same.");
+            using (var user = await MySqlHelper.ExecuteReaderAsync(config.ConnectionString,
+                "Select userID from user WHERE userid = @id and `password` = @currentPass",
+                new MySqlParameter[] { new("@id", user_id), new("@currentPass", currentPass) }))
+            {
+                if (user.Read())
+                {      // if user and current pass combo does't exist
+                    userValues.Add("`password` = @newPass");
+                    parameters.Add(new("@newPass", newPass));
+                }
+                else
+                {
+                    errors.Add("Invalid current password");
+                }
+            }
+        }
+        if (parameters.Count > 1)
+        {
+            query += string.Join(',', userValues) + " WHERE userid = @id";
+            int updatedRows = await MySqlHelper.ExecuteNonQueryAsync(config.ConnectionString, query, parameters.ToArray());
+            if (updatedRows == 0) { status = "error"; errors.Add("Sql failed to update profile"); }
+        }
+        else
+            errors.Add("");
+        return new updateUserDataResult(status, errors.Count()>0? errors.ToArray():null ); // errors array to string
     }
 }
