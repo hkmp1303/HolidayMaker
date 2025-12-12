eb3d4084a615bc666b5ac8ddfb41528743cad35f
M-Renerg <mikael.k.renberg@hotmail.com>
2025-12-12 10:17:21 +0100

    finished writing the code for booking a package
    the code works but need tweeking if you want it to do more.
    added the postman quary that I used for testing.

--- TravelPackage.cs
+++ TravelPackage.cs
@@ -1,5 +1,6 @@
 using System.Reflection.Metadata;
 using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Common;
 using Org.BouncyCastle.Cms;
 using Org.BouncyCastle.Crypto.Engines;
 namespace server;
@@ -10,7 +11,11 @@ namespace server;
 static class PackageBooking
 {
 
-    public record TravelPackage(int transportationid);
+    public record TravelPackage(int travelpackageid, List<Bookinghotel> hotellist, DateTime start, DateTime end);
+
+    public record Bookinghotel(int fk_room_id, DateTime date_start, DateTime date_end);
+
+    public record PackageResult(string packagetitle, string hotelname, string hoteldesc, int idhotel, string activityname, string activitydesc, int idactivity);
 
     public static async Task CreatePackageBooking(TravelPackage credentials, Config config, HttpContext ctx)
     {
@@ -25,64 +30,132 @@ static class PackageBooking
                 using var transaction = con.BeginTransaction();
 
                 
+
                 string checkpackagequery = """                       
-                            SELECT t.title, h.name, h.description, a.name, a.description
+                            SELECT t.title AS packagetitle,
+                            h.name AS hotelname,
+                            h.description AS hoteldesc,
+                            h.hotelid AS idhotel,
+                            a.name AS activityname, 
+                            a.description AS activitydesc,
+                            a.activityid AS idactivity
                             FROM holidaymaker.travelpackage AS t
                             JOIN holidaymaker.packagedetails AS p ON t.travelpackageid = p.fk_travelpackage_id
                             JOIN holidaymaker.hotel AS h ON p.fk_hotel_id = h.hotelid 
                             JOIN holidaymaker.activity AS a ON p.fk_activity_id = a.activityid
                             WHERE t.travelpackageid = @searchpackage;
                             """;
+
+                var result = new List<PackageResult>();
+
                 using(var cmd = new MySqlCommand(checkpackagequery, con, transaction))
                 {
-                    cmd.Parameters.AddWithValue("@searchpackage", credentials.transportationid);
+                    cmd.Parameters.AddWithValue("@searchpackage", credentials.travelpackageid);
 
-                    var result = await cmd.ExecuteReaderAsync();
+                    using var reader = cmd.ExecuteReader();
+
+                    while (await reader.ReadAsync())
+                    {
+                        result.Add(new PackageResult(
+                            reader.GetString("packagetitle"),
+                            reader.GetString("hotelname"),
+                            reader.GetString("hoteldesc"),
+                            reader.GetInt32("idhotel"),
+                            reader.GetString("activityname"),
+                            reader.GetString("activitydesc"),
+                            reader.GetInt32("idactivity"))
+                        );
+                    }
+                    
+                }
+                await ctx.Response.WriteAsJsonAsync(result);   //funkade innan jag skrev resten av koden ........
+
+                // display package?????
+
+                //select room in hotel????
+                //select date_start????
+                //select date_end?????
+
+                // add yes, no logic if you want to continue with the booking????????
+
+                //logic to select transportation????
+
+                int booking_id;
+
+                string bookingquery = """
+                     INSERT INTO booking(fk_user_id, bookingdate)
+                     VALUES(@userid, NOW());
+                     SELECT LAST_INSERT_ID();
+                     """;
+
+                using (var cmd = new MySqlCommand(bookingquery, con, transaction))
+                {
+                    cmd.Parameters.AddWithValue("@userid", user_id);
+
+                    var result_id = await cmd.ExecuteScalarAsync();
+                    booking_id = Convert.ToInt32(result_id);
                 }
 
-                // int booking_id;
+                foreach(var hotel in credentials.hotellist)
+                {
+                    string bookinghotelquary = """
+                    INSERT INTO bookinghotel(fk_booking_id, fk_room_id, date_start, date_end)
+                    VALUES(@fkbookingid, @fkroomid, @date_start, @date_end)
+                    """;
 
-                // using (var cmd = new MySqlCommand(bookingquery, con, transaction))
-                // {
-                //     cmd.Parameters.AddWithValue("@userid", user_id);
-                //     cmd.Parameters.AddWithValue();
-
-                //     var result_id = await cmd.ExecuteScalarAsync();
-                //     booking_id = Convert.ToInt32(result_id);
-                // }
-
+                    using var cmd = new MySqlCommand(bookinghotelquary, con, transaction);
                 
-                
-                // foreach(var hotel in credentials.hotellist)
-                // {
-                //     string bookinghotelquary = """
-                //     INSERT INTO bookinghotel(fk_booking_id, fk_room_id, date_start, date_end)
-                //     VALUES(@fkbookingid, @fkroomid, @date_start, @date_end)
-                //     """;
+                    cmd.Parameters.AddWithValue("@fkbookingid", booking_id);
+                    cmd.Parameters.AddWithValue("@fkroomid", hotel.fk_room_id);
+                    cmd.Parameters.AddWithValue("@date_start", hotel.date_start);
+                    cmd.Parameters.AddWithValue("@date_end", hotel.date_end);
 
-                //     using var cmd = new MySqlCommand(bookinghotelquary, con, transaction);
-                
-                //     cmd.Parameters.AddWithValue("@fkbookingid", booking_id);
-                //     cmd.Parameters.AddWithValue("@fkroomid", hotel.fk_room_id);
-                //     cmd.Parameters.AddWithValue("@date_start", hotel.date_start);
-                //     cmd.Parameters.AddWithValue("@date_end", hotel.date_end);
+                    await cmd.ExecuteNonQueryAsync();
+                }
 
-                //     await cmd.ExecuteNonQueryAsync();
-                // }
 
-                await transaction.CommitAsync();
-                
+                var activity_id = result.Select(r => r.idactivity);
+
+                foreach(var a_id in activity_id)
+                {
+
+                string Bookingactivityquery = """
+                INSERT INTO Bookingactivity(fk_booking_id, fk_activity_id, datestart, dateend)
+                VALUES(@fkbookingid, @fkactivityid, @datestart, @dateend)
+                """;
+
+                using var cmd = new MySqlCommand(Bookingactivityquery, con, transaction);
+
+                cmd.Parameters.AddWithValue("@bookingid", booking_id);
+                cmd.Parameters.AddWithValue("@fkactivityid", a_id);
+                cmd.Parameters.AddWithValue("@datestart", credentials.start);
+                cmd.Parameters.AddWithValue("@dateend", credentials.end);
+                }
+                await transaction.CommitAsync();                
             }
+            
         }
+        
     }
 }
 
 /*
-
-select t.title, h.name, h.description, a.name, a.description 
-from holidaymaker.travelpackage as t
-join holidaymaker.packagedetails as p on t.travelpackageid = p.fk_travelpackage_id
-join holidaymaker.hotel as h on p.fk_hotel_id = h.hotelid 
-join holidaymaker.activity as a on p.fk_activity_id = a.activityid; 
-
+postman quary:
+{
+  "travelpackageid": 1,
+  "start": "2025-07-01",
+  "end": "2025-07-10",
+  "hotellist": [
+    {
+      "fk_room_id": 101,
+      "date_start": "2025-07-01",
+      "date_end": "2025-07-05"
+    },
+    {
+      "fk_room_id": 102,
+      "date_start": "2025-07-05",
+      "date_end": "2025-07-10"
+    }
+  ]
+}
 */
